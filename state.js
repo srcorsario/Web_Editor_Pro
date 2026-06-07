93474b6ff53341e8480afafb9588fb6a6dda536d
Sr Corsario <116904782+srcorsario@users.noreply.github.com>
2026-06-07 11:46:46 +0200

    Update state.js

--- state.js
+++ state.js
@@ -4,8 +4,13 @@ const API_KEYS = [
     "TU_SEGUNDA_API_KEY_AQUI" 
 ];
 
+// Endpoint oficial de Google Apps Script para persistencia multidireccional (A-AA)
+const WEB_APP_URL = 'https://script.google.com/macros/s/AKfycbyDQEGNn8pLpVu-7n4bh6CyzLeKfYeICM0Fbr358SKwVP9rt9yNcc7UV6au2FPMyftiXg/exec';
+
 function getKeys() {
     return API_KEYS;
 }
 
-
+function getWebAppUrl() {
+    return WEB_APP_URL;
+}
